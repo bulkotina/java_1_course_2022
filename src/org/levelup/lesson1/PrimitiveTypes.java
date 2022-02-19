abd970356e78724f6dc01345122aed09fb596f06
bulkotina <bulkotina.olga@yandex.ru>
2022-02-19 22:58:09 +0300

    task1

---
+++ src/org/levelup/lesson1/PrimitiveTypes.java
@@ -0,0 +1,28 @@
+package org.levelup.lesson1;
+
+import sun.awt.geom.AreaOp;
+
+public class PrimitiveTypes {
+    //psvme
+    public static void main (String[] args) {
+        int a;
+        a = 156;
+
+        int b = 756;
+
+        int sum = a + b;
+        System.out.println(sum);
+         char symbol = 'H';
+         char intSymbol = 72;
+        System.out.println(symbol);
+        System.out.println(intSymbol);
+
+        float f1 = 543.34f;
+        float f2 =234.23f;
+        System.out.println(f1 + f2);
+
+        //sout
+        System.out.println("sum " + f1 + f2);
+
+    }
+}
