e078a6b5f9cffae4c98563e3453b5419cf4e6f50
bulkotina <bulkotina.olga@yandex.ru>
2022-02-21 20:49:48 +0300

    task6

---
+++ Task6.java
@@ -0,0 +1,10 @@
+package org.levelup.lesson2;
+
+public class Task6 {
+    public static void main(String[] args) {
+        //Создайте программу, выводящую на экран все четырёхзначные числа последовательности 1000 1003 1006 1009 1012 1015 ….
+        for (int i = 1000; i < 10000; i++) {
+            System.out.print(i + " ");
+        }
+    }
+}
