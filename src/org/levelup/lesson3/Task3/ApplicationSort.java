5260c41af5feb6fad8c98876967866b040af29ce
bulkotina <bulkotina.olga@yandex.ru>
2022-02-27 00:47:53 +0300

    task

--- src/org/levelup/lesson3/Task3/ApplicationSort.java
+++ src/org/levelup/lesson3/Task3/ApplicationSort.java
@@ -6,9 +6,7 @@ public class ApplicationSort {
     public static void main(String[] args) {
         int[] arr = new int[]{-45, 5, 1, 654, 7, 0};
         ArraySorting arrSort = new ArraySorting();
-       // System.out.println(Arrays.toString(
-                arrSort.bubbleSort(arr);
-                //));
+        arrSort.bubbleSort(arr);
         arrSort.printArr(arr);
 
     }
